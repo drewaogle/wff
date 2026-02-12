94f79d60dda3fdcffe680005c512de5332383ffd
Drew Ogle <drew@aperturedata.io>
2026-02-12 11:36:05 -0500

    work on retrieving faces from db

--- db_get.py
+++ db_get.py
@@ -1,15 +1,92 @@
-
+import sys
+import math
 import weaviate
 wc = weaviate.Client( url="http://localhost:8080")
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.axes_grid1 import ImageGrid
+import base64
 
 #https://weaviate-python-client.readthedocs.io/en/v3.2.3/weaviate.data.html
 
 #col = wc.collections.use("Embeddings_vggface_retinaface_aligned_raw")
 first=True
+all_dets=[]
+
+q = wc.query.get("Embeddings_vggface_retinaface_aligned_raw",\
+        properties=["img_name","face","face_shape"])\
+    .with_additional([ "id"]).build()
+print(q)
+
+from pprint import pprint
+r = wc.query.raw(q)
+
+imgs={}
+if "errors" in r:
+    pprint(r)
+    sys.exit(1)
+for itm in r["data"]["Get"]["Embeddings_vggface_retinaface_aligned_raw"]:
+    print(f"* {itm['_additional']['id']} -  {itm['img_name']}")
+    n = itm['img_name']
+    if not n in imgs:
+        imgs[n] = []
+    imgs[n].append( [itm['face'], itm['face_shape']] )
+
+from PIL import Image
+from io import BytesIO
+for imgn in imgs.keys():
+    nf = len(imgs[imgn])
+    noun = "faces" if nf != 1 else "face"
+    print(f"{imgn} - {nf} {noun}")
+    if False:
+        im1 = np.arange(100).reshape((10,10))
+        im2 = im1.T
+        im3 = np.flipud(im2)
+        im4 = np.fliplr(im2)
+
+        fig = plt.figure(figsize=(4.,4.))
+        grid = ImageGrid(fig, 111, nrows_ncols=(2,2),
+                axes_pad=0.1)
+
+        for ax, im in zip(grid,[im1,im2,im3,im4]):
+            ax.imshow(im)
+    else:
+        if nf == 1:
+            nr,nc = [1,1]
+        else:
+            nr=2
+            nc=nf/nr
+            while nc > nr:
+                nr = nr + 1
+                nc=nf/nr
+        nc=math.ceil(nc)
+        print(f"Grid is {nr} rows, {nc} columns")
+        fig = plt.figure(figsize=(nr*1.,nc*1.))
+        grid = ImageGrid(fig,111, nrows_ncols=(nr,nc), axes_pad=0.1)
+        for ax,im in zip(grid,imgs[imgn]):
+            print(type(im[0]))
+            ibin = base64.b64decode(im[0])
+            print(type(ibin))
+            print(im[1])
+            nar = np.frombuffer(ibin,dtype='<u4')
+            #nar = np.array(list(ibin),dtype=int)
+            nar = nar.reshape( im[1] )
+            nar = nar[:,:,::-1]
+            as_pil = Image.frombuffer( 'RGB', im[1][:2], nar.flatten()  ) # BytesIO(ibin))
+
+            ax.imshow( as_pil ) #np.array(ibin))
+
+    plt.show()
+    sys.exit(1)
+
+
+
 for itm in wc.data_object.get()['objects']:
     #print(itm.uuid, itm.properties)
     if first:
         first=False
         print( "Props: {}".format( ",".join( itm['properties'].keys())))
-    print(f"* {itm['class']} {itm['id']}")
+    props = itm['properties']
+    # props face?
+    #print(f"* {itm['class']} {props['img_name']} {props['face_shape']} {itm['id']}")
     #"objects" ?
--- test_deepface.py
+++ test_deepface.py
@@ -34,53 +34,8 @@ if __name__ == "__main__":
 
     tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
     with tf.device('/GPU:0') :
-        from deepface.modules import representation as df_repr
-        from deepface.modules import modeling as df_mdl
-        from deepface.modules import detection as df_det
-        from deepface.modules import preprocessing as df_pre
-        # check if model loads
-        if test_load:
-            print("Try load")
-            mod = df_mdl.build_model(task="facial_recognition", model_name="VGG-Face")
-            ts=mod.input_shape
-            norml="base"
-            print("Loaded") 
-
-        # if it does, do extract faces.
         for f in img_dir.iterdir():
             if f.is_file():
 
-                if test_load:
-                    imgs = df_det.extract_faces( img_path=f, grayscale=False,
-                            enforce_detection=True, align=True, expand_percentage=0,
-                            anti_spoofing=False,max_faces=None)
-
-                    fimgs=[]
-                    print(f"Testing {len(imgs)} faces")
-                    for im in imgs:
-                        bgrimg = im["face"][:,:,::-1]
-                        rsz = df_pre.resize_image(
-                                img=bgrimg,target_size=(ts[1],ts[0]))
-                        fimg = df_pre.normalize_input(img=rsz,normalization=norml)
-                        fimgs.append(fimg)
-                    batch_ims = np.concatenate(fimg,axis=0)
-
-                    print(f"number dimensions = {batch_ims.ndim} and shape 0 is {batch_ims.shape[0]}")
-
-                    print("GGGGGGGGGGGGGGGGGGGGGGGGGOOO")
-                    if batch_ims.ndim == 3:
-                        batch_ims = np.expand_dims(batch_ims,axis=0)
-                    ems = mod.model.predict_on_batch(batch_ims) 
-                    #ems = mod.model(batch_ims,training=False)
-                    #ems = mod.forward(batch_ims)
-                    print("went")
-
-                    raise Exception("bye")
-                #print("REPR")
-                #df_repr.represent( f , model_name="ArcFace", detector_backend='retinaface' )
-                #print(f"* {f}")
-                #os.environ["DEEPFACE_WEAVIATE_URI"] = "http://localhost:8080"
-                #wc = Client("http://localhost:8080")
-                DeepFace.register(img = f,database_type="weaviate",
-                        connection=wc, detector_backend= 'retinaface')
-                        # , connection_details="https://localhost:50051")
+                print(f"* Analyzing {f.name}")
+                res= DeepFace.register(img = f, img_name=f.name,database_type="weaviate", connection=wc, detector_backend= 'retinaface')
